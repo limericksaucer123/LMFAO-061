7736a9b0db24d8eda2ae8f7c25fa4e51a996dbd8
limericksaucer123 <limericksaucer123@gmail.com>
2026-02-09 23:44:40 +0000

    fied admin panel

--- .eleventy.js
+++ .eleventy.js
@@ -2,12 +2,12 @@
 
 module.exports = function (eleventyConfig) {
   // Passthrough copy for static assets
-  eleventyConfig.addPassthroughCopy("src/css");
-  eleventyConfig.addPassthroughCopy("src/js");
-  eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("./src/css");
+  eleventyConfig.addPassthroughCopy("./src/js");
+  eleventyConfig.addPassthroughCopy("./src/images");
 
   // Passthrough copy for admin folder (Decap CMS)
-  eleventyConfig.addPassthroughCopy("src/admin");
+  eleventyConfig.addPassthroughCopy("./src/admin");
 
   return {
     dir: {
